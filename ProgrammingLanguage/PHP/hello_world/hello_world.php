1612fe14516b91d9327a176e088df8faa67df955
JackDrogon <jack.xsuperman@gmail.com>
2014-07-10 10:14:49 +0800

    Add hello_world in PHP

---
+++ ProgrammingLanguage/PHP/hello_world/hello_world.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Hello PHP </title>
+	</head>
+	<body>
+		<?php echo "<p> Hello,World! </p> <br />" ?>
+		<?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+	</body>
+</html>
