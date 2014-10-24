0cdd90e38c34397054dc7e203f67dd54fd7a3596
JackDrogon <jack.xsuperman@gmail.com>
2014-10-24 19:04:58 +0800

    Change ProgrammingLanguage/PHP/hello_world/hello_web.php

--- ProgrammingLanguage/PHP/hello_world/hello_web.php
+++ ProgrammingLanguage/PHP/hello_world/hello_web.php
@@ -1,6 +1,42 @@
 <?php
-	echo "Hello,World!\n";
+	echo "Hello,World!<br />\n";
+
+	// for
 	for ($i = 0; $i < 10; ++$i) {
 		echo $i . "\n";
 	}
+	echo "<br />\n";
+
+	// while
+	$i = 100;
+	while ($i--) {
+		echo $i . "\n";
+	}
+	echo "<br />\n";
+
+	// if -- else
+	$i = 2;
+	if ($i%2) {
+		echo $i . " is odd.<br />\n";
+	} else {
+		echo $i . " is odd.<br />\n";
+	}
+
+	// if -- elseif -- else
+
+	// switch
+	$i = 2;
+	switch ($i) {
+		case 0:	echo "i is 0.<br />\n";
+			break;
+		case 1:	echo "i is 1.<br />\n";
+			break;
+		case 2:	echo "i is 2.<br />\n";
+			break;
+		default:
+			echo "No value.<br />\n";
+	}
+
+	// foreach
+	// class
 ?>
