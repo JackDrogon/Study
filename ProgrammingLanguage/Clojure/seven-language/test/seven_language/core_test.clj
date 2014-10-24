3f999092009a76b58b48638c0f9f6ab8ef921aab
JackDrogon <jack.xsuperman@gmail.com>
2014-10-24 20:12:08 +0800

    Init Clojure

---
+++ ProgrammingLanguage/Clojure/seven-language/test/seven_language/core_test.clj
@@ -0,0 +1,7 @@
+(ns seven-language.core-test
+  (:require [clojure.test :refer :all]
+            [seven-language.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
